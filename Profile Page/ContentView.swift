48e740d2a0fab98b874b9ba83ca834523a92b8e5
Muhammad <96905615+sud0pacman@users.noreply.github.com>
2026-09-11 00:16:50 +0500

    Initial Commit

---
+++ Profile Page/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Profile Page
+//
+//  Created by Muhammad on 11/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Profile Page/Profile_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_PageApp.swift
+//  Profile Page
+//
+//  Created by Muhammad on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Profile_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
